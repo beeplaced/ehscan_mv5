4d650812a5295dcf6998d3364924a6cd59ce00ef
beeplaced <admin@beeplace.de>
2024-12-01 17:02:33 +0100

    1

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'my-app-cache-v1.07'; // Update version when there are changes
+const CACHE_NAME = 'my-app-cache-v1.08'; // Update version when there are changes
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html'
